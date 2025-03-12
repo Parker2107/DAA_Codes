5fa6453f7f57fac3a5fc9c0273aadf7297869504
Parker2107 <113683584+Parker2107@users.noreply.github.com>
2025-03-12 12:59:47 +0530

    KMP algorithm

---
+++ KMP.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int *prefixTable(char *p)
+{
+    int m = strlen(p);
+    int *LPS = (int *)malloc(m * sizeof(int));
+    LPS[0] = 0;
+    int k = 0;
+    for (int q = 1; q < m; q++)
+    {
+        while (k > 0 && p[k] != p[q])
+        {
+            k = LPS[k];
+        }
+        if (p[k] == p[q])
+        {
+            k++;
+        }
+        LPS[q] = k;
+    }
+    for (int i = 0; i < m; i++)
+    {
+        printf("%d ", LPS[i]);
+    }
+    printf("\n");
+    return LPS;
+}
+
+void matcher(char *t, char *p)
+{
+    int n = strlen(t);
+    int m = strlen(p);
+    int *LPS = prefixTable(p);
+    int q = 0;
+    for (int i = 0; i < n; i++)
+    {
+        while (q > 0 && p[q] != t[i])
+        {
+            q = LPS[q - 1];
+        }
+        if (p[q] == t[i])
+        {
+            q++;
+        }
+        if (q == m)
+        {
+            printf("Pattern matches starting at %d\n", i - m + 2);
+            q = LPS[q - 1];
+        }
+    }
+}
+int main(void)
+{
+    char *t = "abceabcdabeabcdabcdabde";
+    char *p = "abcdabd";
+    matcher(t, p);
+    return 1;
+}
